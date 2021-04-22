0a56ff7494ceb8c2c959c535776d05b3db607eea
ayebravo <67244374+ayebravo@users.noreply.github.com>
2021-04-22 16:40:12 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import DictIcon from "./media/DictIcon.png";
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <img src={DictIcon} id="dictIcon" alt="dictionary icon" className="img-fluid" />
+      </header>
+    </div>
+  );
+}
