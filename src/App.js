cc810c70879ae26453d510d194631bff45a8612e
ayebravo <67244374+ayebravo@users.noreply.github.com>
2021-05-25 18:06:04 -0500

    Created Dictionary component with a form
    Added some initial styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import DictIcon from "./media/DictIcon.png";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          <img src={DictIcon} id="dictIcon" alt="dictionary icon" className="img-fluid" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+            <h1 className="heading">Dictionary</h1>
+            <img src={DictIcon} id="dictIcon" alt="dictionary icon" className="img-fluid" />
+        </header>
+        <main className="App-main">
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+            <small>Coded by Ayelen Bravo</small>
+        </footer>
+      </div>
     </div>
   );
 }
